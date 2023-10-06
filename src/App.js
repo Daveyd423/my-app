b8b4a669f2e9110cc7fc6109ca08bd41441ad7f5
Daveyd423 <everythingethics101@gmail.com>
2023-10-06 11:11:56 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DB is the best football player named DB at csn
         </a>
       </header>
     </div>
